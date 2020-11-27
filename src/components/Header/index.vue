e4462aa9040316d51f7edaaffd0696ebcec67482
su991215 <1049859435@qq.com>
2020-11-27 21:11:23 +0800

    更新了路由操作

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -9,8 +9,8 @@
             <p>尚品汇欢迎您！</p>
             <p>
               <span>请</span>
-              &nbsp; <a href="###">登录</a>
-              <a href="###" class="register">免费注册</a>
+              &nbsp; <router-link to="/login">登录</router-link>
+              <router-link to="/register" class="register">免费注册</router-link>
             </p>
           </div>
           <div class="typeList">
@@ -28,9 +28,9 @@
       <!--头部第二行 搜索区域-->
       <div class="bottom">
         <h1 class="logoArea">
-          <a class="logo" title="尚品汇" href="###" target="_blank">
+          <router-link to="/" class="logo" title="尚品汇">
             <img src="./images/logo.png" alt="" />
-          </a>
+          </router-link>
         </h1>
         <div class="searchArea">
           <form action="###" class="searchForm">
